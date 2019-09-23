4655ed62bb6f2061946d418699a3f930b65444be
Matthew Klawitter <mtklaw153@gmail.com>
2019-09-23 19:27:34 -0400

    Started Plugin interface implementation

--- telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/BotPlugin.java
+++ telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/BotPlugin.java
@@ -1,6 +1,21 @@
 package cafe.seafarers.plugins;
 
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
+
 public interface BotPlugin {
+    /**
+     * Run every time a user types a command in Telegram mapped to this plugin
+     * @return BaseRequest a Telegram object to be sent to a chat room
+     */
+    public BaseRequest onCommand(Update update);
+
+    /**
+     * Returns a list of commands that when received by Telegram run this plugin's onCommand method
+     * @return Array String
+     */
+    public String[] getCommands();
+
     /**
      * The name of the plugin
      * @return String plugin name
