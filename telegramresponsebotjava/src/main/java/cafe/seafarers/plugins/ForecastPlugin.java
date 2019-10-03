0dee5db873313d1328dcd1fa97b92f94c43027d7
Mark Powers <markppowers0@gmail.com>
2019-10-03 14:10:47 -0500

    Make Forecast Plugin work

--- telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/ForecastPlugin.java
+++ telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/ForecastPlugin.java
@@ -1,15 +1,27 @@
 package cafe.seafarers.plugins;
 
-import com.pengrad.telegrambot.model.InlineQuery;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.Gson;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.KeyboardButton;
-import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
-import com.pengrad.telegrambot.request.AnswerInlineQuery;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 
+import cafe.seafarers.config.Resources;
+
 public class ForecastPlugin implements BotPlugin {
 	private static final String[] COMMANDS = { "forecast" };
+	private static String apiKey;
 
 	@Override
 	public BaseRequest onCommand(Update update) {
@@ -18,14 +30,124 @@ public class ForecastPlugin implements BotPlugin {
 		String args = message.substring(command.length()).trim();
 		switch (command) {
 		case "forecast":
-			KeyboardButton[] kbs = {new KeyboardButton("Send location")};
-			kbs[0].requestLocation(true);
-			//AnswerInlineQuery i = new AnswerInlineQuery(args, null);
-			SendMessage m = new SendMessage(update.message().chat().id(), "Please send location");
-			m.replyMarkup(new ReplyKeyboardMarkup(kbs));
-			return m;
+			if (args.isEmpty()) {
+				return new SendMessage(update.message().chat().id(), "Please enter where (City,country)");
+			}
+			// TODO request location if none supplied?
+//			KeyboardButton[] kbs = {new KeyboardButton("Send location")};
+//			kbs[0].requestLocation(true);
+//			//AnswerInlineQuery i = new AnswerInlineQuery(args, null);
+//			SendMessage m = new SendMessage(update.message().chat().id(), "Please send location");
+//			m.replyMarkup(new ReplyKeyboardMarkup(kbs));
+			return new SendMessage(update.message().chat().id(), weather(args));
 		}
 		return null;
+	}
+
+	// Modeling http response
+	private class Response {
+		Coord coord;
+		Weather[] weather;
+		String base;
+		Main main;
+		int visibility;
+		Wind wind;
+		Clouds clouds;
+		Rain rain;
+		Snow snow;
+		int dt;
+		Sys sys;
+		int timezone;
+		int id;
+		String name;
+		int cod;
+
+		@Override
+		public String toString() {
+			StringBuffer sb = new StringBuffer();
+			sb.append("In ");
+			sb.append(name);
+			sb.append(" it is ");
+			sb.append(main.temp);
+			sb.append(" degrees with ");
+			sb.append(weather[0].description);
+			return sb.toString();
+		}
+	}
+
+	private class Coord {
+		double lat;
+		double lon;
+	}
+
+	private class Weather {
+		int id;
+		String main;
+		String description;
+		String icon;
+	}
+
+	private class Main {
+		double temp;
+		double pressure;
+		double humidity;
+		double temp_min;
+		double temp_max;
+	}
+
+	private class Wind {
+		double speed;
+		int deg;
+	}
+
+	private class Clouds {
+		double all;
+	}
+
+	private class Rain {
+		// int 1h;
+		// int 3h;
+	}
+
+	private class Snow {
+		// int 1h;
+		// int 3h;
+	}
+
+	private class Sys {
+		int type;
+		int id;
+		double message;
+		String country;
+		int sunrise;
+		int sunset;
+	}
+
+	public String weather(String place) {
+		String baseUrl = "https://api.openweathermap.org/data/2.5/";
+		String method = "weather?q=" + place;
+		String key = "&APPID=" + apiKey;
+		String params = "&units=imperial";
+		String requestUrl = baseUrl + method + key + params;
+
+		URL myurl;
+		try {
+			myurl = new URL(requestUrl);
+			HttpsURLConnection con = (HttpsURLConnection) myurl.openConnection();
+			con.setRequestMethod("GET");
+
+			StringBuffer sb = new StringBuffer();
+			DataInputStream input = new DataInputStream(con.getInputStream());
+			for (int c = input.read(); c != -1; c = input.read()) {
+				sb.append((char) c);
+			}
+			input.close();
+			Response r = new Gson().fromJson(sb.toString(), Response.class);
+			return r.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "Invalid location";
+		}
 	}
 
 	@Override
@@ -55,6 +177,15 @@ public class ForecastPlugin implements BotPlugin {
 
 	@Override
 	public boolean enable() {
+		File f = Resources.LoadFile(this, "config.txt");
+		Scanner fileIn;
+		try {
+			fileIn = new Scanner(f);
+			apiKey = fileIn.nextLine().trim();
+			fileIn.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		return true;
 	}
 
