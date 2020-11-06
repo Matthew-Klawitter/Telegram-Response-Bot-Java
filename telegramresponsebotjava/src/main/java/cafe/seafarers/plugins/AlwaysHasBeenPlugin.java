75ba52af8940e51c31fb0edf2464055631834604
Matthew Klawitter <mtklaw153@gmail.com>
2020-11-05 21:10:28 -0500

    Add AlwaysHasBeenPlugin as a fun entertainment plugin

---
+++ telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/AlwaysHasBeenPlugin.java
@@ -0,0 +1,118 @@
+package cafe.seafarers.plugins;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import com.pengrad.telegrambot.model.BotCommand;
+import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.BaseRequest;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendPhoto;
+
+import cafe.seafarers.config.Resources;
+
+public class AlwaysHasBeenPlugin implements BotPlugin {
+
+    private static final Color TEXT_COLOR = new Color(255, 255, 255);
+    private static final int[] COORDS = { 100, 150, 370, 90};
+
+    private HashSet<String> collectedWords;
+    private Random random;
+    private boolean isEnabled;
+
+    @Override
+    public BaseRequest onCommand(Update update) {
+        return null;
+    }
+
+    @Override
+    public BaseRequest onMessage(Update update) {
+        if (isEnabled){
+            File file = Resources.LoadFile(this, "always.jpg");
+            try {
+                String message = update.message().text();
+                if (message.toLowerCase().startsWith("wait, it's ") || message.toLowerCase().startsWith("wait it's") ||
+                        message.toLowerCase().startsWith("wait, its ") || message.toLowerCase().startsWith("wait its ")) {
+
+                    BufferedImage bufferedImage = ImageIO.read(file);
+                    Graphics graphics = bufferedImage.getGraphics();
+
+                    graphics.setColor(TEXT_COLOR);
+                    graphics.setFont(new Font("Arial", Font.PLAIN, 18));
+                    int x1 = COORDS[0];
+                    int y1 = COORDS[1];
+                    graphics.drawString(message, x1, y1);
+
+                    graphics.setFont(new Font("Arial", Font.PLAIN, 32));
+                    int x2 = COORDS[2];
+                    int y2 = COORDS[3];
+                    graphics.drawString("Always has been.", x2, y2);
+
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    ImageIO.write(bufferedImage, "jpg", out);
+                    return new SendPhoto(update.message().chat().id(), out.toByteArray());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public BotCommand[] getCommands() {
+        return new BotCommand[0];
+    }
+
+    @Override
+    public String getName() {
+        return "AlwaysHasBeen";
+    }
+
+    @Override
+    public String getAuthor() {
+        return "Matthew Klawitter";
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0";
+    }
+
+    @Override
+    public boolean enable() {
+        isEnabled = true;
+        return true;
+    }
+
+    @Override
+    public boolean disable() {
+        isEnabled = false;
+        return true;
+    }
+
+    @Override
+    public boolean hasMessageAccess() {
+        return true;
+    }
+
+    @Override
+    public String getHelp() {
+        return "Returns a captioned meme when someone says 'Wait, it's'";
+    }
+
+    @Override
+    public BaseRequest periodicUpdate() {
+        return null;
+    }
+}
--- telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/SocialCreditPlugin.java
+++ telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/SocialCreditPlugin.java
@@ -4,10 +4,7 @@ import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.BaseRequest;
 
-public class TimerPlugin implements BotPlugin {
-    private static final String[] COMMANDS = { "timer" };
-    private static final String[] DESCRIPTIONS = { "'/timer [set,view,delete] [name] [date]' to set, view, and delete a timer." };
-
+public class SocialCreditPlugin implements BotPlugin {
     @Override
     public BaseRequest onCommand(Update update) {
         return null;
@@ -20,11 +17,7 @@ public class TimerPlugin implements BotPlugin {
 
     @Override
     public BotCommand[] getCommands() {
-        BotCommand[] botCommands = new BotCommand[COMMANDS.length];
-        for (int i = 0; i < botCommands.length; i++) {
-            botCommands[i] = new BotCommand(COMMANDS[i], DESCRIPTIONS[i]);
-        }
-        return botCommands;
+        return new BotCommand[0];
     }
 
     @Override
@@ -39,22 +32,22 @@ public class TimerPlugin implements BotPlugin {
 
     @Override
     public String getName() {
-        return "Timer";
+        return null;
     }
 
     @Override
     public String getAuthor() {
-        return "Matthew Klawitter";
+        return null;
     }
 
     @Override
     public String getVersion() {
-        return "V0.1";
+        return null;
     }
 
     @Override
     public String getHelp() {
-        return "Helps you schedule countdowns between different dates and times.";
+        return null;
     }
 
     @Override
