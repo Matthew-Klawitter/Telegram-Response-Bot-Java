59a208ad199acafad1a6fd14fd7a192e1fd9f5ee
Matthew Klawitter <mtklaw153@gmail.com>
2021-06-14 16:30:52 -0400

    Rebalanced payout, enabled wins for vertical and diagonal matches

--- telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/SlotsPlugin.java
+++ telegramresponsebotjava/src/main/java/cafe/seafarers/plugins/SlotsPlugin.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class SlotsPlugin implements BotPlugin {
-    private final int COST = 3;
+    private final int COST = 5;
     private final String[] COMMANDS = {"slots"};
     private final String[] DESCRIPTIONS = {String.format("spend $%d to play roulette", COST)};
 
@@ -33,15 +33,13 @@ public class SlotsPlugin implements BotPlugin {
 
             StringBuilder sb = new StringBuilder();
             sb.append(String.format("%s inserts %d coins and pulls the crank...", user, COST));
-            sb.append(System.getProperty("line.separator")).append(System.getProperty("line.separator"));
-            sb.append("The slots display the following:").append(System.getProperty("line.separator"));
-            sb.append(g.getRolledPattern()[0]).append(System.getProperty("line.separator"));
-            sb.append(g.getRolledPattern()[1]).append(System.getProperty("line.separator"));
-            sb.append(g.getRolledPattern()[2]).append(System.getProperty("line.separator"));
-            sb.append(System.getProperty("line.separator"));
+            sb.append(System.lineSeparator()).append(System.lineSeparator());
+            sb.append("The slots display the following:").append(System.lineSeparator());
+            sb.append(g.toString()).append(System.lineSeparator());
+            sb.append(System.lineSeparator());
 
             if (g.payout > 0){
-                sb.append("Wow, you're a big winner!").append(System.getProperty("line.separator"));
+                sb.append("Wow, you're a big winner!").append(System.lineSeparator());
                 sb.append("You won: $").append(g.payout);
                 BankManager.deposit(user, g.payout);
             }
@@ -110,72 +108,121 @@ public class SlotsPlugin implements BotPlugin {
 
     class SlotGame {
         private HashMap<String, Integer> validPatterns;
-        private String[] rolledPattern;
+        private char[][] rolledPatterns;
         private int payout;
 
         public SlotGame(){
             validPatterns = configurePatternValues();
-            rolledPattern = rollSlots();
-            payout = checkWinnings(rolledPattern, validPatterns);
+            rolledPatterns = rollSlots();
+            payout = checkCrossWinnings(rolledPatterns, validPatterns) +
+                            checkCrossWinnings(getTransposedSquareMatrix(rolledPatterns), validPatterns) +
+                            checkDiagonalWinnings(rolledPatterns, validPatterns);
         }
 
         private HashMap<String, Integer> configurePatternValues(){
             HashMap<String, Integer> p = new HashMap<>();
-            p.put("777", 100);
-            p.put("$$$", 50);
-            p.put("@@@", 25);
+            p.put("777", 80);
+            p.put("$$$", 35);
+            p.put("@@@", 15);
             p.put("***", 10);
             p.put("xxx", 5);
 
             return p;
         }
 
-        private String[] rollSlots(){
+        private char[][] rollSlots(){
             Random r = new Random();
-            String[] p = new String[3];
+            char[][] p = new char[3][3];
 
-            for (int i = 0; i < 3; i++){
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < 3; j++){
+            for (int i = 0; i < p.length; i++){
+                for (int j = 0; j < p[i].length; j++){
                     int roll = r.nextInt(100) + 1;
 
                     if (95 < roll & roll <= 100){ // 5%
-                        sb.append("7");
+                        p[i][j] = '7';
                     }
                     else if (80 < roll & roll <= 95){ // 15%
-                        sb.append("$");
+                        p[i][j] = '$';
                     }
                     else if (60 < roll && roll <= 80){ // 20%
-                        sb.append("@");
+                        p[i][j] = '@';
                     }
                     else if (35 < roll && roll <= 60){ // 25%
-                        sb.append("*");
+                        p[i][j] = '*';
                     }
                     else { // 35%
-                        sb.append("x");
+                        p[i][j] = 'x';
                     }
                 }
-
-                p[i] = sb.toString();
             }
 
             return p;
         }
 
-        private int checkWinnings(String[] pattern, HashMap<String, Integer> valid){
-            int totalWinnings = 0;
+        private int checkCrossWinnings(char[][] pattern, HashMap<String, Integer> valid){
+            int winnings = 0;
 
-            for (int i = 0; i < pattern.length; i++){
-                if (valid.containsKey(pattern[i])){
-                    totalWinnings += valid.get(pattern[i]);
+            for (int row = 0; row < pattern.length; row++){
+                StringBuilder sb = new StringBuilder();
+                for (int col = 0; col < pattern[row].length; col++){
+                    sb.append(pattern[row][col]);
+                }
+
+                String rolledPattern = sb.toString();
+                if (valid.containsKey(rolledPattern)){
+                    winnings += valid.get(rolledPattern);
                 }
             }
 
-            return totalWinnings;
+            return winnings;
         }
 
-        public String[] getRolledPattern() {
-            return rolledPattern;
+        private int checkDiagonalWinnings(char[][] pattern, HashMap<String, Integer> valid){
+            int winnings = 0;
+
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < pattern.length; row++){
+                sb.append(pattern[row][row]);
+            }
+
+            if (valid.containsKey(sb.toString())){
+                winnings += valid.get(sb.toString());
+            }
+
+            sb.delete(0, sb.length());
+            for (int row = 0; row < pattern.length; row++){
+                sb.append(pattern[row][pattern.length - row - 1]);
+            }
+
+            if (valid.containsKey(sb.toString())){
+                winnings += valid.get(sb.toString());
+            }
+
+            return winnings;
+        }
+
+        private char[][] getTransposedSquareMatrix(char[][] pattern){
+            char[][] transposedMatrix = new char[pattern[0].length][pattern.length];
+
+            for (int i = 0; i < pattern[0].length; i++){
+                for (int j = 0; j < pattern.length; j++){
+                    transposedMatrix[i][j] = pattern[j][i];
+                }
+            }
+
+            return transposedMatrix;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < rolledPatterns.length; i++){
+                for (int j = 0; j < rolledPatterns.length; j++){
+                    sb.append(rolledPatterns[i][j]);
+                }
+                sb.append(System.lineSeparator());
+            }
+            return sb.toString();
         }
 
         public int getPayout() {
